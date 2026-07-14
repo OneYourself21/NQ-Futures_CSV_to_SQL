ebce56d0e88840c8e85210ab05e49eb550fd2053
One <rajahaider08@hotmail.co.uk>
2026-07-14 09:20:36 -0400

    Added: testing for certain days with counts and added another function that returns a bunch of holidays

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import sqlite3
+import datetime as dt
 import pandas as pd
+
+#timestampET = 0
+#open = 1
+#high = 2
+#low = 3
+#close = 4
+#volume = 5
+#Vwap_RTH = 6
+#Vwap_ETH = 7
 
 def csv_to_db():
     df = pd.read_csv('Dataset_NQ_1min_2022_2025.csv')
@@ -38,5 +48,110 @@ def csv_to_db():
     connection.close()
 
 
+def read_date_to_date(starting_date : dt.datetime, ending_date : dt.datetime) -> list:
+    connection = sqlite3.connect('NQ_Futures.db')
+    cursor = connection.cursor()
+
+    starting_date = starting_date.strftime('%Y-%m-%d %H:%M:%S')
+    ending_date = ending_date.strftime('%Y-%m-%d %H:%M:%S')
+
+    cursor.execute(''' SELECT * FROM NQ_1m WHERE timestampET >= ? AND timestampET <= ?;
+    ''', (starting_date, ending_date))
+
+    rows = cursor.fetchall()
+
+    connection.close()
+    return rows
+
+
+def zero_volume_test() -> int:
+    connection = sqlite3.connect('NQ_Futures.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''SELECT *
+                      FROM NQ_1m
+                      WHERE volume == 0;''')
+
+    rows = cursor.fetchall()
+
+    connection.close()
+    return len(rows)
+
+
+def hour_count_test() -> list:
+    connection = sqlite3.connect('NQ_Futures.db')
+    cursor = connection.cursor()
+
+    hours_amount = []
+
+    cursor.execute('''SELECT COUNT(*) 
+                          FROM NQ_1m
+                          GROUP BY strftime('%H', timestampET);''')
+
+    counts = cursor.fetchall()
+    for i in range(24):
+        print(f"Hour {i} length:  {counts[i][0]}")
+
+
+    hours_amount.extend(counts)
+
+    cursor.execute(''' SELECT COUNT(*) 
+                       FROM NQ_1m
+                       WHERE strftime('%H:%M', timestampET) == '17:00';''')
+    five_vol = cursor.fetchone()[0]
+    print(f"Note that number of 17:00 candles is {five_vol}.")
+
+    connection.close()
+    return hours_amount
+
+
+def candle_per_day_test() -> dict:
+    n_dict = {}
+
+    connection = sqlite3.connect('NQ_Futures.db')
+    cursor = connection.cursor()
+
+    cursor.execute(''' SELECT COUNT(*) 
+                       FROM NQ_1m
+                       GROUP BY strftime('%Y-%m-%d', timestampET, '+6 hours');''')
+
+    counts = cursor.fetchall()
+    for count in counts:
+        if n_dict.keys().__contains__(count[0]):
+            n_dict[count[0]] += 1
+
+        else:
+            n_dict.update({count[0]: 1})
+
+    connection.close()
+    return n_dict
+
+def test_a_day(n : int) -> list:
+    connection = sqlite3.connect('NQ_Futures.db')
+    cursor = connection.cursor()
+
+    cursor.execute(''' SELECT timestampET
+                           FROM NQ_1m
+                           GROUP BY strftime('%Y-%m-%d', timestampET, '+6 hours')
+                           HAVING COUNT(*) >= ?;''', (n,))
+
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+
+def holidays() -> list:
+    connection = sqlite3.connect('NQ_Futures.db')
+    cursor = connection.cursor()
+
+    cursor.execute(''' SELECT timestampET
+                       FROM NQ_1m
+                       GROUP BY strftime('%Y-%m-%d', timestampET, '+6 hours')
+                       HAVING (COUNT(*) >= 1000) AND (COUNT(*)) <= 1300;''')
+
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
 if __name__ == '__main__':
-    csv_to_db()
+    test_a_day(930)
